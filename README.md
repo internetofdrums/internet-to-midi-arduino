2fa2886242f9c7616a1e3cd7c2e619acdfeee8eb
Piet van Dongen <piet.vandongen@luminis.eu>
2018-03-25 19:54:12 +0200

    First version without note length

---
+++ README.md
@@ -0,0 +1,6 @@
+# Internet Drum Pattern to MIDI Converter (Arduino)
+
+Arduino software for [Internet Drum Pattern][1] to MIDI converter hardware. 
+
+[1]: https://github.com/internetofdrums/internet-drum-pattern-spec#readme
+
---
+++ internet-to-midi-arduino.ino
@@ -0,0 +1,124 @@
+const unsigned int kLatchPin = 8;
+const unsigned int kClockPin = 12;
+const unsigned int kDataPin = 11;
+const unsigned long kMidiBaudRate = 31250;
+const unsigned short int kSecondsPerMinute = 60;
+const unsigned short int kMilliSecondsPerSecond = 1000;
+const unsigned char kBeatsPerMinute = 120;
+const unsigned char kBeatsPerBar = 4;
+const unsigned char kSubdivisionsPerBeat = 4;
+const unsigned char kSubdivisionsPerBar = kBeatsPerBar * kSubdivisionsPerBeat;
+const unsigned char kLedPins[4] = {13, 12, 8, 7};
+const unsigned char kNumberOfLeds = sizeof(kLedPins) / sizeof(*kLedPins);
+const signed char kMidiNoteOnCommand = 0x99; // Channel 10 note on
+const signed char kMidiNoteOffCommand = 0x89; // Channel 10 note off
+const signed char kLowestNote = 0x24; // Key 36 / C1 / Bass Drum 1
+
+unsigned short int kMilliSecondsPerSubdivision = (((float) kSecondsPerMinute / (float) kBeatsPerMinute) * kMilliSecondsPerSecond) / 4;
+unsigned int kMilliSecondsPerBar = kSubdivisionsPerBar * kMilliSecondsPerSubdivision;
+
+unsigned long milliSecondsPassed = 0;
+unsigned char lastPlayedSubdivision = kSubdivisionsPerBar - 1; // The last subdivision of the bar
+
+signed char pattern[16][16] = {
+  {0x7F, 0x00, 0x7F, 0x7F,   0x7F, 0x00, 0x7F, 0x7F,   0x7F, 0x00, 0x7F, 0x7F,   0x7F, 0x00, 0x7F, 0x7F,}, // Bass drum
+  {0x00, 0x00, 0x00, 0x00,   0x7F, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x7F, 0x00, 0x00, 0x00,}, 
+  {0x00, 0x00, 0x00, 0x00,   0x7F, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x7F, 0x00, 0x00, 0x00,}, // Snare
+  {0x00, 0x00, 0x7F, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,}, // Clap
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,}, 
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x7F, 0x00, 0x7F,   0x00, 0x7F, 0x00, 0x7F,   0x00, 0x7F, 0x00, 0x7F,   0x00, 0x7F, 0x00, 0x7F,}, // Hihat
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,},
+  {0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,}
+};
+const unsigned char kNumberOfInstruments = sizeof(pattern) / sizeof(*pattern);
+
+void setup() {
+  Serial.begin(kMidiBaudRate);
+  
+  //pinMode(kLatchPin, OUTPUT);
+  //pinMode(kClockPin, OUTPUT);
+  //pinMode(kDataPin, OUTPUT);
+}
+
+void loop() {
+  milliSecondsPassed = millis();
+  
+  if (shouldPlaySubdivision()) {
+    playSubdivision();
+  }
+}
+
+bool shouldPlaySubdivision() {
+  unsigned char closestPassedSubdivision = floor((milliSecondsPassed % kMilliSecondsPerBar) / (float) kMilliSecondsPerSubdivision);
+
+  return closestPassedSubdivision != lastPlayedSubdivision;
+}
+
+bool shouldShowLed(char velocity) {
+  return velocity > 0;
+}
+
+void playSubdivision() {
+  unsigned char currentSubdivision = (lastPlayedSubdivision + 1) % kSubdivisionsPerBar; 
+  unsigned int data = 0;
+  
+  // Send note-on message for each note, collect LED information
+  for (char note = 0; note < kNumberOfInstruments; note++) {
+    signed char velocity = pattern[note][currentSubdivision];
+    
+    sendMidiNoteOn(note, velocity);
+    
+    if (shouldShowLed(velocity)) {
+      bitSet(data, note);
+    }
+  }
+  
+  //showLeds(data);
+  
+  delay(kMilliSecondsPerSubdivision / 2);
+  
+  // Send note off message for each note
+  for (char note = 0; note < kNumberOfInstruments; note++) {
+    signed char velocity = pattern[note][currentSubdivision];
+    
+    sendMidiNoteOff(note, velocity);
+  }
+  
+  lastPlayedSubdivision = currentSubdivision;
+}
+
+void sendMidiNoteOn(signed char note, signed char velocity) {
+  sendMidiMessage(kMidiNoteOnCommand, note, velocity);
+}
+
+void sendMidiNoteOff(signed char note, signed char velocity) {
+  sendMidiMessage(kMidiNoteOffCommand, note, velocity);
+}
+
+void sendMidiMessage(signed char command, signed char note, signed char velocity) {
+  Serial.write(command);
+  Serial.write(kLowestNote + note);
+  Serial.write(velocity);
+}
+
+void showLeds(unsigned int data) {
+  // Get the first byte of the data
+  byte data01 = data & 0xff;
+  
+  // Get the second byte of the data
+  byte data02 = (data >> 8) & 0xff;
+  
+  digitalWrite(kLatchPin, LOW);
+  shiftOut(kDataPin, kClockPin, MSBFIRST, data02);
+  shiftOut(kDataPin, kClockPin, MSBFIRST, data01);
+  digitalWrite(kLatchPin, HIGH);
+}
+
